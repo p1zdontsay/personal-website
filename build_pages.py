1a2465a2ddbfe021a55d17c7687e85769934a9f2
Yichen Xu <yx2613@columbia.edu>
2026-09-16 17:28:34 +0000

    Add one more photo to the Iceland collection

--- build_pages.py
+++ build_pages.py
@@ -494,7 +494,7 @@ photography_body = """
 """ + place("Altay", "altay-5.jpg", "photo.place.altay", "Altay",
             [f"altay-{n}.jpg" for n in range(1, 10) if n != 5]) + """
 """ + place("Iceland", "iceland-2.jpg", "photo.place.iceland", "Iceland",
-            [f"iceland-{n}.jpg" for n in range(1, 9) if n != 2]) + """
+            [f"iceland-{n}.jpg" for n in range(1, 10) if n != 2]) + """
       </div>
 
       <h3 class="subhead" data-i18n="photo.video.title">Video</h3>
