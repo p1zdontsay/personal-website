7e602f45667c5f152f540c6f44f895b34e5ae7e5
Yichen Xu <yx2613@columbia.edu>
2026-09-16 17:44:39 +0000

    Switch Hawaii cover photo to the Milky Way night sky shot

--- build_pages.py
+++ build_pages.py
@@ -500,8 +500,8 @@ photography_body = """
       <h3 class="subhead" data-i18n="photo.photo.title">Photo</h3>
       <p class="lede" data-i18n="photo.photo.lede">Hover or tap a place to see a few frames from there &mdash; click one to view it larger. More places coming.</p>
       <div class="place-grid">
-""" + place("Hawaii", "hawaii-11.jpg", "photo.place.hawaii", "Hawaii",
-            [f"hawaii-{n}.jpg" for n in range(1, 17) if n != 11]) + """
+""" + place("Hawaii", "hawaii-7.jpg", "photo.place.hawaii", "Hawaii",
+            [f"hawaii-{n}.jpg" for n in range(1, 17) if n != 7]) + """
 """ + place("Altay", "altay-5.jpg", "photo.place.altay", "Altay",
             [f"altay-{n}.jpg" for n in range(1, 10) if n != 5]) + """
 """ + place("Iceland", "iceland-1.jpg", "photo.place.iceland", "Iceland",
